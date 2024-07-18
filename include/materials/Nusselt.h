125f638d0e89f06617e6f5592608178717558783
Nathan Glaser <nglaser3@illinois.edu>
2024-07-18 13:44:19 -0500

    Add updated Nusselt Number, removed vector vel

--- include/materials/Nusselt.h
+++ include/materials/Nusselt.h
@@ -16,18 +16,20 @@ public:
 protected:
   virtual void computeQpProperties() override;
   Real computeSpeed();
-  Real _k;
+
+  MaterialProperty<Real> _k;
+  MaterialProperty<Real> _pr;
+  MaterialProperty<Real> _kin_visc;
+
   Real _l_value;
   MooseEnum _correlation; 
-  Real _pr;
-  Real _kin_visc;
-  bool _vec_velocity;
-  const VectorVariableValue * const _velocity;
+
   const VariableValue * const _u_vel;
   const VariableValue * const _v_vel;
   const VariableValue * const _w_vel;
+  
   MaterialProperty<Real> & _re;
   MaterialProperty<Real> & _nu;
   MaterialProperty<Real> & _h;
-  //using T::_velocity;
+
 };
--- src/materials/Nusselt.C
+++ src/materials/Nusselt.C
@@ -6,18 +6,15 @@ InputParameters
 Nusselt::validParams()
 {
   InputParameters params = Material::validParams();
-  params.addRequiredParam<Real>("thermal_conductivity", 
-                        "Thermal conductivity of fluid");
+  params.addRequiredParam<std::string>("thermal_conductivity", 
+                                      "Thermal conductivity of fluid");
   params.addRequiredParam<Real>("L", "The channel radius");
+  params.addParam<std::string>("prandtl_number","Prandtl number of fluid");
+  params.addParam<std::string>("kinematic_viscosity",
+  "The kinematic viscosity of the fluid, dynamic viscosity divided by the density");
   MooseEnum nusselt_correlation("Krepel Dittus-Boelter", "Krepel");
   params.addParam<MooseEnum>("nusselt_correlation",nusselt_correlation,
-                             "Which correlation to use to approximate the Nusselt number");
-  params.addParam<Real>("prandtl_number",10.0,"Prandtl number of fluid");
-  params.addParam<Real>("kinematic_viscosity",1.0e-6,
-                        "The kinematic viscosity of the fluid, dynamic viscosity divided by the density");
-  params.addParam<bool>("vector_velocity",false,
-                        "Whether the velocity variable is a vector or in components, true for vector");
-  params.addCoupledVar("velocity","vector variable holding velocity");
+          "Which correlation to use to approximate the Nusselt number");
   params.addCoupledVar("u_velocity","scalar variable holding u component of velocity");
   params.addCoupledVar("v_velocity","scalar variable holding v component of velocity");
   params.addCoupledVar("w_velocity","scalar variable holding w component of velocity");
@@ -26,20 +23,21 @@ Nusselt::validParams()
 
 Nusselt::Nusselt(const InputParameters & parameters)
   : Material(parameters),
-    _k(getParam<Real>("thermal_conductivity")),
+    _k(getMaterialProperty<Real>(getParam<std::string>("thermal_conductivity"))),
     _l_value(getParam<Real>("L")),
+    _pr(getMaterialProperty<Real>(getParam<std::string>("prandtl_number"))),
+    _kin_visc(getMaterialProperty<Real>(getParam<std::string>("kinematic_viscosity"))),
     _correlation(getParam<MooseEnum>("nusselt_correlation")),
-    _pr(getParam<Real>("prandtl_number")),
-    _kin_visc(getParam<Real>("kinematic_viscosity")),
-    _vec_velocity(getParam<bool>("vector_velocity")),
-    _velocity(((getParam<MooseEnum>("nusselt_correlation") != "Krepel") && (getParam<bool>("vector_velocity"))) ? &coupledVectorValue("velocity") : nullptr),
-    _u_vel(((getParam<MooseEnum>("nusselt_correlation") != "Krepel") && !(getParam<bool>("vector_velocity"))) ? &coupledValue("u_velocity") : nullptr),
-    _v_vel(((getParam<MooseEnum>("nusselt_correlation") != "Krepel") && !(getParam<bool>("vector_velocity"))) ? &coupledValue("v_velocity") : nullptr),
-    _w_vel(((getParam<MooseEnum>("nusselt_correlation") != "Krepel") && !(getParam<bool>("vector_velocity"))) ? &coupledValue("w_velocity") : nullptr),
     _re(declareProperty<Real>("ReynoldsNumber")),
     _nu(declareProperty<Real>("NusseltNumber")),
     _h(declareProperty<Real>("HeatTransferCoeff"))
 {
+  if (_correlation != "Krepel")
+  {
+    _u_vel(&coupledValue("u_velocity"));
+    _v_vel(&coupledValue("v_velocity"));
+    _w_vel(&coupledValue("w_velocity"));
+  }
 }
 
 /* assumes velocity is an elemental variable
@@ -48,14 +46,7 @@ Nusselt::Nusselt(const InputParameters & parameters)
 */
 Real Nusselt::computeSpeed()
 {
-  if (_vec_velocity)
-  {
-    return std::pow(std::pow((*_velocity)[_qp](0),2)+std::pow((*_velocity)[_qp](1),2)+std::pow((*_velocity)[_qp](2),2),.5);
-  }
-  else
-  {
     return std::pow(std::pow((*_u_vel)[_qp],2) + std::pow((*_v_vel)[_qp],2) + std::pow((*_w_vel)[_qp],2),0.5);
-  }
 }
 
 void
@@ -63,8 +54,8 @@ Nusselt::computeQpProperties()
 {
   if (_correlation=="Dittus-Boelter")
   {
-    _re[_qp] = Nusselt::computeSpeed() * _l_value / _kin_visc;
-    _nu[_qp] = 0.023 * std::pow(_re[_qp],0.8) * std::pow(_pr,0.4);
+    _re[_qp] = computeSpeed() * _l_value / _kin_visc[_qp];
+    _nu[_qp] = 0.023 * std::pow(_re[_qp],0.8) * std::pow(_pr[_qp],0.4);
   };
   if (_correlation=="Krepel")
   {
@@ -72,5 +63,5 @@ Nusselt::computeQpProperties()
     _re[_qp] = 0.; //place holder for no reynolds
     _nu[_qp] = 4.;
   };
-  _h[_qp] = _nu[_qp] * _k / _l_value;
+  _h[_qp] = _nu[_qp] * _k[_qp] / _l_value;
 }
