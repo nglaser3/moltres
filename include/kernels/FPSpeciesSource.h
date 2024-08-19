61f723cdc30cf84c0963f0c547008a1b93151e85
Nathan Glaser <nglaser3@illinois.edu>
2024-08-19 13:04:32 -0400

    Add FPSPeciesSource kernel

---
+++ include/kernels/FPSpeciesSource.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "Kernel.h"
+#include "ScalarTransportBase.h"
+
+class FPSpeciesSource : public Kernel, public ScalarTransportBase
+{
+
+public:
+    FPSpeciesSource(const InputParameters & parameters);
+
+    static InputParameters validParams();
+
+protected:
+    virtual Real computeQpResidual() override;
+    virtual Real computeQpJacobian() override;
+    virtual Real computeQpOffDiagJacobian(unsigned int jvar) override;
+
+    unsigned int _num_groups;
+    const std::vector<Real> _gamma;
+    const VariableValue & _temp;
+    unsigned int _temp_id;
+    const MaterialProperty<std::vector<Real>> & _nsf;
+    const MaterialProperty<std::vector<Real>> & _d_nsf_d_temp;
+    
+    std::vector<const VariableValue*> _group_fluxs;
+    std::vector<unsigned int> _flux_ids;
+};
---
+++ src/kernels/FPSpeciesSource.C
@@ -0,0 +1,82 @@
+#include "FPSpeciesSource.h"
+
+ registerMooseObject("MoltresApp",FPSpeciesSource);
+
+ InputParameters
+ FPSpeciesSource::validParams()
+ {
+    InputParameters params = Kernel::validParams();
+    params += ScalarTransportBase::validParams();
+    params.addRequiredParam<unsigned>("num_groups",
+    "Number of group fluxes in simulation");
+    params.addRequiredCoupledVar("group_fluxes", 
+    "All the variables that hold the group fluxes."
+    "These MUST be listed by decreasing" 
+    "energy/increasing group number.");
+    params.addRequiredParam<std::vector<Real>>("fission_yield",
+    "Fission yield, gamma, of species for each energy group.");
+    params.addCoupledVar("temperature", 800, 
+    "The temperature used to interpolate material properties.");
+    return params;
+ }
+
+ FPSpeciesSource::FPSpeciesSource(const InputParameters & parameters)
+   : Kernel(parameters),
+     ScalarTransportBase(parameters),
+     _num_groups(getParam<unsigned>("num_groups")),
+     _gamma(getParam<std::vector<Real>>("fission_yield")),
+     _temp(coupledValue("temperature")),
+     _temp_id(coupled("temperature")),
+     _nsf(getMaterialProperty<std::vector<Real>>("nsf")),
+     _d_nsf_d_temp(getMaterialProperty<std::vector<Real>>("d_nsf_d_temp"))
+     
+
+{
+    
+    _group_fluxs.resize(_num_groups);
+    _flux_ids.resize(_num_groups);
+    for (unsigned int i = 0; i < _num_groups; ++i)
+    {
+        _group_fluxs[i] = &coupledValue("group_fluxes", i);
+        _flux_ids[i] = coupled("group_fluxes", i);
+    }
+}
+
+Real
+FPSpeciesSource::computeQpResidual()
+{
+  Real r = 0;
+  for (unsigned int i = 0; i < _num_groups; ++i)
+  {
+    r += -_test[_i][_qp] *_gamma[i] * _nsf[_qp][i] * computeConcentration((*_group_fluxs[i]),_qp);
+  }
+    return r;
+}
+
+Real
+FPSpeciesSource::computeQpJacobian()
+{
+  return 0.0; //decay has no temperature dependence, neither does fission yield
+}
+
+
+Real
+FPSpeciesSource::computeQpOffDiagJacobian(unsigned int jvar)
+{
+  Real jac = 0.0;
+  for (unsigned int i = 0; i < _num_groups; ++i)
+    if (jvar == _flux_ids[i])
+    {
+      jac = -_test[_i][_qp] * _gamma[i] * _nsf[_qp][i] *
+            computeConcentrationDerivative((*_group_fluxs[i]), _phi, _j, _qp);
+      break;
+    }
+
+  if (jvar == _temp_id)
+  {
+    for (unsigned int i = 0; i < _num_groups; ++i)
+      jac += -_test[_i][_qp] *_gamma[i] * _d_nsf_d_temp[_qp][i] * 
+      _phi[_j][_qp] * computeConcentration((*_group_fluxs[i]), _qp);
+  }
+  return jac;
+}
