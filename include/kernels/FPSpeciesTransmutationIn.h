8ad1c265f6d1777a59c87057d42e4a3282b05f12
Nathan Glaser <nglaser3@illinois.edu>
2024-08-19 13:05:22 -0400

    Add Transmutation source kernel

---
+++ include/kernels/FPSpeciesTransmutationIn.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "ScalarTransportBase.h"
+
+class FPSpeciesTransmutationIn : public Kernel, public ScalarTransportBase
+{
+
+public:
+    static InputParameters validParams();
+    FPSpeciesTransmutationIn(const InputParameters & parameters);
+
+protected:
+    virtual Real computeQpResidual() override;
+    virtual Real computeQpJacobian() override;
+    virtual Real computeQpOffDiagJacobian(unsigned int jvar) override;
+
+    std::vector<std::string> _parent_zaids;
+    int _num_parents;
+    int _num_groups;
+
+    bool _transmute;
+    bool _decay;
+
+    std::vector<const VariableValue*> _parent_concs;
+    std::vector<unsigned int> _parent_ids;
+    std::vector<const VariableValue*> _group_fluxs;
+    std::vector<unsigned int> _flux_ids;
+};
---
+++ src/kernels/FPSpeciesTransmutationIn.C
@@ -0,0 +1,102 @@
+#pragma once
+
+#include "FPSpeciesTransmutationIn.h"
+
+registerMooseObject("MoltresApp", FPSpeciesTransmutationIn);
+
+InputParameters 
+FPSpeciesTransmutationIn::validParams()
+{
+    params = Kernel::validParams();
+    params += ScalarTransportBase::validParams();
+    params.addRequiredParam<std::vector<std::string>>(
+    "parents_identifiers", "The parent identifier, in ZAID format."
+    "Multiply the atomic number, Z, by 1000; and then add the atomic mass, A."
+    "For example, Xe-135 : 54135; C-12 : 06012.");
+    params.addParam<std::vector<Real>>("decay_constant", 
+    "The decay constant of the parents that yields the species of interest."
+    "This is the decay constant of the parents multiplied by the branching"
+    "fraction and factor that yield the species of interest.")
+    params.addCoupledVar("group_fluxes", 
+    "All the variables that hold the group fluxes."
+    "These MUST be listed by decreasing" 
+    "energy/increasing group number.");
+    params.addParam<unsigned>("num_groups",
+    "Number of group fluxes in simulation");
+    params.addParam<int>("num_groups",
+    "The number of energy groups in the simulation")
+    params.addCoupledVar("parent_concentrations",
+    "The variable(s) holding the concentrations of the"
+    "DIRECT parents of the species of interest.");
+    params.addCoupledVar("temperature", 800, 
+    "The temperature used to interpolate material properties.");
+}
+
+FPSpeciesTransmutationIn::FPSpeciesTransmutationIn(const InputParameters & parameters)
+  : Kernel(parameters),
+    ScalarTransportBase(parameters),
+    _parent_zaids(getParam<std::vector<std::string>>("parent_identifiers")),
+    _num_parents(_parent_zaids.size()),
+    _num_groups(getParam<int>("num_groups"))
+    _transmute(isValidParam("group_fluxes")),
+    _decay(isValidParam("decay_constant"))
+{
+    _parent_concs.resize(_num_parents);
+    _parent_ids.resize(_num_parents);
+    for (unsigned int i = 0; i < _num_parents; ++i)
+    {
+        _parent_concs[i] = &coupledValue("parent_concentrations", i);
+        _parent_ids[i] = coupled("parent_concentrations", i);
+    }
+
+    if (_transmute)
+    {
+        _group_fluxs.resize(_num_groups);
+        _flux_ids.resize(_num_groups);
+        for (unsigned int i = 0; i < _num_groups; ++i)
+        {
+            _group_fluxs[i] = &coupledValue("group_fluxes", i);
+            _flux_ids[i] = coupled("group_fluxes", i);
+        }
+    }
+    if (!_decay)
+    {
+        _lambda = getParam<std::vector<Real>>("decay_constant");
+    }
+    if (!_decay && !_transmute)
+    {
+        mooseError("Parent must either decay or transmute into the species of interest. 
+        If decay is desired, please provide the decay_constant parameter. If transmutation
+        is desired, please provide the group_fluxes and num_groups parameter, ASWELL as
+        insure the material block 'FileSpeciesTransmute' is in the simulation!");
+    }
+}
+
+Real
+FPSpeciesTransmutationIn::computeQpResidual()
+{
+    Real res = 0.0;
+    for (int i = 0; i < num_parents; i++)
+    {
+        if (_decay)
+        {
+            res += _test[_i][_qp]*_lambda * computeConcentration((*_parent_concs[i]),_qp);
+        }
+        if (_transmute)
+        {
+            for (int j = 0; j < num_groups; j++)
+            {
+                res +=  _test[_i][_qp] * _microxs[i] * computeConcentration((*parent_concs[i]),_qp) 
+            * computeConcentration((*_group_fluxes[j]),_qp);
+            }
+        }
+    }
+}
+
+Real 
+FPSpeciesTransmutationIn::computeQpJacobian()
+{
+    return 0.0;
+}
+
+
