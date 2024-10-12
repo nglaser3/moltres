77c69597ac3a381d158e4a4f9f0a9788ddf78139
Nathan Glaser <nglaser3@illinois.edu>
2024-10-12 12:13:18 -0500

    Initial construction of speciestrackmaterial

---
+++ include/materials/SpeciesTrackMaterial.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include "GenericConstantMaterial.h"
+#include "nlohmann/json.h"
+#include <fstream>
+#include <vector>
+#include <map>
+
+class SpeciesTrackMaterial : public GenericConstantMaterial
+{
+public:
+    SpeciesTrackMaterial(const InputParameters & parameters);
+    static InputParameters validParams();
+
+protected:
+    virtual void ComputeQpProperties();
+
+    std::string _filename;
+    std::string _nuclide;
+    MaterialProperty<Real> & _lambda;
+    MaterialProperty<Real> & _fis_yield;
+    MaterialProperty<std::vector<Real>> _abs_xs;
+    MaterialProperty<std::map<std::string,std::tuple<Real,Real>>> _dp_props;
+    MaterialProperty<std::map<std::string,std::vector<Real>>> _tp_props;
+};
---
+++ src/materials/SpeciesTrackMaterial.C
@@ -0,0 +1,57 @@
+#pragma once
+
+#include "SpeciesTrackMaterial"
+
+using json = nlohmann::json;
+using namespace std;
+
+InputParameters
+SpeciesTrackMaterial::validParams()
+{
+    InputParameters params = GenericConstantMaterial::validParams();
+    params.addRequiredParam<string>("filepath",
+                "The file path of the json file constructed 
+                with moltres_xs.speciestracking.")
+    params.addRequiredParam<string>("Isotope",
+                "The isotope this material will store properties for.")
+}
+
+SpeciesTrackMaterial::SpeciesTrackMaterial(const InputParameters & parameters)
+    : GenericConstantMaterial(parameters), 
+    _filename(getParam<string>("filepath")),
+    _nuclide(getParam<string>("isotope")),
+    _lambda(declareProperty<Real>(_nuclide+"_lambda")),
+    _fis_yield(declareProperty<Real>(_nuclide+"_fisyield")),
+    _abs_xs(declareProperty<vector<Real>>(_nuclide+"_absxs")),
+    _dp_props(declareProperty<map<string, tuple<Real,Real>>>(_nuclide+"_dp_data")),
+    _tp_props(declareProperty<map<string,vector<Real>>>(_nuclide+"_tp_data"))
+{       
+}
+
+void 
+SpeciesTrackMaterial::ComputeQpProperties()
+{
+    std::ifstream file(_filename);
+    json data = json::parse(file);
+
+    json decay = data[_nuclide]["decay"];
+    auto dpars = decay["parents"].template get<vector<string>>();
+    auto dp_lam = decay["parent_lambdas"].template get<vector<Real>>();
+    auto dp_bra = decay["parent_branching"].template get<vector<Real>>();
+    for (int i =0; i < dpars.size(); i++)
+    {
+        tuple _p_data(dp_lam[i],dp_bra[i]);
+        _dp_props[_qp][dpars[i]] = _p_data;
+    }
+    _lambda[_qp] = decay["lambda"].template get<Real>();
+
+    json transmute = data[_nuclide]["transmute"];
+    auto tpars = transmute["parents"].template get<map<string,vector<Real>>>();
+    for (auto it = tpars.begin(); it != tpars.end(); it++)
+    {
+        _tp_props[_qp][it->first] = it->second;
+    }
+    _abs_xs[_qp] = transmute["abs_xs"].template get<vector<Real>>();
+    _fis_yield[_qp] = transmute["fission_yield"].template get<Real>();
+
+}
