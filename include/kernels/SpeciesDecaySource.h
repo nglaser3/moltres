b0ae87c0a43c51bab4a5d241f2d055c3b4da3c58
Nathan Glaser <nglaser3@illinois.edu>
2024-10-13 15:29:31 -0500

    Finalize decay source

---
+++ include/kernels/SpeciesDecaySource.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#include "ScalarTransportBase.h"
+#include "Kernel.h"
+
+class SpeciesDecaySource : public Kernel, public ScalarTransportBase
+{
+public:
+    SpeciesDecaySource(const InputParameters & parameters);
+    static InputParameters validParams();
+
+protected:
+    virtual Real computeQpResidual() override;
+    virtual Real computeQpJacobian() override;
+    virtual Real computeQpOffDiagJacobian(unsigned int jvar) override;
+
+    std::vector<std::string> _parents;
+    unsigned int _num_parents;
+
+    const MaterialProperty<std::unordered_map<std::string,std::vector<Real>>> & _dp_map;
+
+    std::vector<const VariableValue*> _parent_concs;
+    std::vector<unsigned int> _parent_ids;
+};
---
+++ src/kernels/SpeciesDecaySource.C
@@ -0,0 +1,78 @@
+#pragma once
+
+#include "SpeciesDecaySource.h"
+
+registerMooseObject("MoltresApp",SpeciesDecaySource);
+
+InputParameters
+SpeciesDecaySource::validParams()
+{
+    InputParameters params = Kernel::validParams();
+    params += ScalarTransportBase::validParams();
+    params.addRequiredCoupledVar("parent_concs",
+    "All the variables that hold the" 
+    "decay parent concentrations. These MUST"
+    "be listed in the same order as in parent_names.");
+    params.addRequiredParam<std::vector<std::string>>("parent_names",
+    "All decay parent names, MUST be listed in"
+    "the same order as parent_concs");
+    params.addRequiredParam<MaterialPropertyName>("decay_parent_material",
+    "Material holding all parent decay properties");
+    return params;
+}
+
+SpeciesDecaySource::SpeciesDecaySource(const InputParameters & parameters)
+    : Kernel(parameters),
+      ScalarTransportBase(parameters),
+      _parents(getParam<std::vector<std::string>>("parent_names")),
+      _num_parents(_parents.size()),
+      _dp_map(getMaterialProperty<std::unordered_map<std::string,std::vector<Real>>>("decay_parent_material"))
+{
+    _parent_concs.resize(_num_parents);
+    _parent_ids.resize(_num_parents);
+    for (unsigned i = 0; i < _num_parents; ++i)
+    {
+        _parent_concs[i] = &coupledValue("parent_concs",i);
+        _parent_ids[i] = coupled("parent_concs",i);
+    }
+
+}
+
+Real
+SpeciesDecaySource::computeQpResidual()
+{
+    Real res = 0.0;
+    std::unordered_map<std::string,std::vector<Real>> _map = _dp_map[_qp];
+
+    for (int i = 0; i < _num_parents; i++)
+    {
+        res += _test[_i][_qp] * _map[_parents[i]][0]*_map[_parents[i]][1]
+        * computeConcentration((*_parent_concs[i]),_qp);
+    }
+    return res;
+}
+
+Real 
+SpeciesDecaySource::computeQpJacobian()
+{
+    return 0.0;
+}
+
+Real 
+SpeciesDecaySource::computeQpOffDiagJacobian(unsigned int jvar)
+{
+    Real jac = 0.0;
+    std::unordered_map<std::string,std::vector<Real>> _map = _dp_map[_qp];
+
+    for (int i = 0; i < _num_parents; i++)
+    {
+        if (jvar == _parent_ids[i])
+        {
+            jac -= _test[_i][_qp] * _map[_parents[i]][0]
+            * _map[_parents[i]][1] 
+            * computeConcentrationDerivative((*_parent_concs[i]),_phi,_j,_qp);
+            break;
+        }
+    }
+    return jac;
+}
